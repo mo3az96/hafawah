0e3d13a4e185bc591e8924752dbc4420777bf0db
Moaz Mohamed <mo3az.abdellatif@gmail.com>
2023-04-20 00:55:10 +0200

    test

---
+++ js/date.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  var currentDate = moment().format("DD/MM/YYYY");
+  $("#from, #to").daterangepicker(
+    {
+      locale: {
+        format: "DD/MM/YYYY",
+      },
+
+      alwaysShowCalendars: true,
+      minDate: currentDate,
+      autoApply: true,
+      autoUpdateInput: false,
+    },
+    function (start, end) {
+      var selectedStartDate = start.format("DD/MM/YYYY");
+      var selectedEndDate = end.format("DD/MM/YYYY");
+
+      $checkinInput = $("#from");
+      $checkoutInput = $("#to");
+
+      $checkinInput.val(selectedStartDate).trigger("change");
+      $checkoutInput.val(selectedEndDate).trigger("change");
+
+      var checkOutPicker = $checkoutInput.data("daterangepicker");
+      checkOutPicker.setStartDate(selectedStartDate);
+      checkOutPicker.setEndDate(selectedEndDate);
+
+      var checkInPicker = $checkinInput.data("daterangepicker");
+      checkInPicker.setStartDate(selectedStartDate);
+      checkInPicker.setEndDate(selectedEndDate);
+    }
+  );
+  $("#from, #to").on("apply.daterangepicker", function (ev, picker) {
+    var dif = Math.abs(picker.endDate - picker.startDate);
+    d = dif / (1000 * 3600 * 24);
+    $(".days-count").remove();
+    if (document.dir == "rtl") {
+      var text = "(إقامة لمدة " + Math.trunc(d) + " ليالى)";
+    } else {
+      var text = "(" + Math.trunc(d) + " night stay)";
+    }
+    $(".daterangepicker ").append(`<div class='days-count'>
+    <div class="days-range">${picker.startDate.format(
+      "YYYY MM DD"
+    )} - ${picker.endDate.format("YYYY MM DD")}</div>
+    <div class="days-range-count">${text}</div>
+    </div>`);
+  });
+
+  $(".advsearch-date").change(function (e) {
+    if ($(this).val() == "") {
+      $(this)
+        .prev(".advsearch-item-text")
+        .find("span")
+        .text($(this).attr("placeholder"));
+    } else {
+      $(this).prev(".advsearch-item-text").find("span").text($(this).val());
+    }
+  });
+});
