568f23c91fad57c58270a670db77991c8cae7e40
Moaz Mohamed <mo3az.abdellatif@gmail.com>
2023-04-25 19:17:24 +0200

    font

--- js/date.js
+++ js/date.js
@@ -64,18 +64,29 @@ $(document).ready(function () {
     }
   );
   $("#from, #to").on("apply.daterangepicker", function (ev, picker) {
+    var start = picker.startDate._d;
+    var end = picker.endDate._d;
+    var options = {
+      day: "2-digit",
+      year: "numeric",
+      month: "long",
+      weekday: "short",
+    };
     var dif = Math.abs(picker.endDate - picker.startDate);
     d = dif / (1000 * 3600 * 24);
     $(".days-count").remove();
+
     if (document.dir == "rtl") {
       var text = "(إقامة لمدة " + Math.trunc(d) + " ليالى)";
+      var startFormat = start.toLocaleDateString("ar-EG", options);
+      var endFormat = end.toLocaleDateString("ar-EG", options);
     } else {
       var text = "(" + Math.trunc(d) + " night stay)";
+      var startFormat = start.toLocaleDateString("en-US", options);
+      var endFormat = end.toLocaleDateString("en-US", options);
     }
     $(".daterangepicker ").append(`<div class='days-count'>
-    <div class="days-range">${picker.startDate.format(
-      "YYYY MM DD"
-    )} - ${picker.endDate.format("YYYY MM DD")}</div>
+    <div class="days-range">${startFormat} - ${endFormat}</div>
     <div class="days-range-count">${text}</div>
     </div>`);
   });
